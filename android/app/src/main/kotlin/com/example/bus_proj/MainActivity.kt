f2cb04ffef5fc1fcf8ba8e524861d117041dc2df
Nishmakv <nishmakv3@gmail.com>
2024-02-15 14:36:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bus_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bus_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      
+    );
+  }
+}
